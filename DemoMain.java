06bb619f458a1c609ce59b3f2d83d58640ef130e
Atharvkachare <104071489+Atharvkachare@users.noreply.github.com>
2024-04-02 20:37:31 +0530

    Add files via upload

---
+++ DemoMain.java
@@ -0,0 +1,12 @@
+public class DemoMain {
+    public static void main(String[] args) {
+
+        Company comp = new Company();
+
+        Producer p =new Producer(comp);
+        Consumer c = new Consumer(comp);
+        p.start();
+        c.start();
+    }
+    
+}
